637e6814e87348d68e57a1ae7f8020f971be9a25
Vadim Anisimov <i5555450122@gmail.com>
2023-07-14 17:16:58 +0300

    employers handler

---
+++ framework/handlers/employers.py
@@ -0,0 +1,24 @@
+from framework.clients.api import ApiClient
+
+
+class EmployersClient(ApiClient):
+    path = "/employers"
+
+    @staticmethod
+    def get_employers(
+        status_code: int = 200,
+        text: str | None = None,
+        area: int | None = None,
+        only_with_vacancies: bool | None = None,
+        page: int = 0,
+        per_page: int = 100,
+    ):
+        params = {
+            "text": text,
+            "area": area,
+            "only_with_vacancies": only_with_vacancies,
+            "page": page,
+            "per_page": per_page,
+        }
+        data = EmployersClient.get(path=EmployersClient.path, status_code=status_code, params=params)
+        return data.json()
