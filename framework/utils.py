d1e8f1230865d9c8caa921b2aad92c6fa2bbec87
unknown <you@example.com>
2023-11-20 06:19:16 -0800

    typo fix

--- framework/utils.py
+++ framework/utils.py
@@ -6,7 +6,7 @@ def random_word() -> str:
     return wonderwords.RandomWord().word()
 
 
-def morph_word_to_prepositinal_case(word: str) -> str:
+def morph_word_to_prepositional_case(word: str) -> str:
     word = word.split("(", 1)[0][:-1] if "(" in word else word
     prepositional_case = CasesClient.get_prepositional_case(word=word)
     expected_prepositional_case = "в " + prepositional_case.get("П")
